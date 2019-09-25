3bf6be04a4b13701b419c4eedbe8770cda75ac7e
Matteo Kofler <matteokofler1@gmail.com>
2019-09-25 22:22:58 +0200

    injecting css via jquery

--- src/js/content.js
+++ src/js/content.js
@@ -3,13 +3,38 @@ console.log("content.js is here");
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-      fetch(chrome.extension.getURL('src/popup.html'))
+
+      var path = chrome.runtime.getURL('src/css/style.css');
+      $('head').append($('<link>')
+          .attr("rel","stylesheet")
+          .attr("type","text/css")
+          .attr("href", path));
+       path = chrome.runtime.getURL('src/css/bootstrap.min.css');
+      $('head').append($('<link>')
+          .attr("rel","stylesheet")
+          .attr("type","text/css")
+          .attr("href", path));
+          path = chrome.runtime.getURL('src/js/jquery.min.js');
+         $('body').append($('<script>')
+             .attr("type","text/javascript")
+             .attr("src", path));
+
+      path = chrome.runtime.getURL('src/js/bootstrap.min.js');
+     $('body').append($('<script>')
+         .attr("type","text/javascript")
+         .attr("src", path));
+
+         path = chrome.runtime.getURL('src/js/popper.js');
+        $('body').append($('<script>')
+            .attr("type","text/javascript")
+            .attr("src", path));
+
+
+      fetch(chrome.runtime.getURL('src/popup.html'))
           .then(response => response.text())
           .then(data => {
               document.body.innerHTML += data;
-              // other code
-              // eg update injected elements,
-              // add event listeners or logic to connect to other parts of the app
+
           }).catch(err => {
               // handle error
           });
