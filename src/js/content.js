0a2f87cfa421a5e409c507ddbb69ee7ecd2b1d98
Matteo Kofler <matteokofler1@gmail.com>
2019-09-26 18:52:32 +0200

    dialog shows up, bg darkens when clicked

--- src/js/content.js
+++ src/js/content.js
@@ -2,42 +2,30 @@ console.log("content.js is here");
 
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
 
-      var path = chrome.runtime.getURL('src/css/style.css');
-      $('head').append($('<link>')
-          .attr("rel","stylesheet")
-          .attr("type","text/css")
-          .attr("href", path));
-       path = chrome.runtime.getURL('src/css/bootstrap.min.css');
-      $('head').append($('<link>')
-          .attr("rel","stylesheet")
-          .attr("type","text/css")
-          .attr("href", path));
-          path = chrome.runtime.getURL('src/js/jquery.min.js');
-         $('body').append($('<script>')
-             .attr("type","text/javascript")
-             .attr("src", path));
-
-      path = chrome.runtime.getURL('src/js/bootstrap.min.js');
-     $('body').append($('<script>')
-         .attr("type","text/javascript")
-         .attr("src", path));
-
-         path = chrome.runtime.getURL('src/js/popper.js');
-        $('body').append($('<script>')
-            .attr("type","text/javascript")
-            .attr("src", path));
-
-
-      fetch(chrome.runtime.getURL('src/popup.html'))
-          .then(response => response.text())
-          .then(data => {
-              document.body.innerHTML += data;
-
-          }).catch(err => {
-              // handle error
-          });
-     console.log(request);  //log in general site inspector
-    sendResponse({"farewell":"hola"});
-  });
+        var path = chrome.runtime.getURL('src/css/bootstrap.min.dialog.css');
+        $('head').append($('<link>')
+            .attr("rel", "stylesheet")
+            .attr("type", "text/css")
+            .attr("href", path));
+        /* inject jquery, didnt work like that, now in popup.html:
+        path = chrome.runtime.getURL('src/js/jquery.min.js');
+        $('body').prepend($('<script>')
+            .attr("type", "text/javascript")
+            .attr("src", path));*/
+        fetch(chrome.runtime.getURL('src/popup.html'))
+            .then(response => response.text())
+            .then(data => {
+                $(  "body" ).prepend(data);
+                // Show dialog:
+                location.href="javascript:showWishwizModal(); void 0";
+                console.log("modal called");
+            }).catch(err => {
+                // handle error
+            });
+        console.log(request); //log in general site inspector
+        sendResponse({
+            "farewell": "hola"
+        });
+    });
